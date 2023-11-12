cca25c2e32d110087d32ffeb0593f38f16efb2b8
ybuiko <juliabuiko@gmail.com>
2023-11-11 22:01:04 -0500

    Landscaper App: user can use scissors to cut grass

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 const prompt = require("prompt-sync")();
 
 //create function to use tolls to cut garss -- teeth
-const tools = ["teeth"];
+const tools = ["teeth", "scissors"];
 let bankAccount = 0;
 
 console.log(
   " I am starting my landstpe businesss and I can cut your grass with my teath"
 );
 
-function useTeeth() {
-  let answer = prompt("would you like to have your grass cut with my teeth?");
-  console.log(answer);
-  if (answer.toLowerCase() === "y") {
-    bankAccount++;
+function useTool(count, tool) {
+  while (bankAccount < count) {
+    let answer = prompt(`Would you like to have your grass cut with ${tool}?`);
+    console.log(answer);
+    if (answer.toLowerCase() === "y") {
+      bankAccount++;
+    }
   }
   console.log(bankAccount);
 }
 
-function buyTools(price) {
+function buyTools(price, tool) {
   if (bankAccount >= 5) {
-    let answer = prompt("would you like to buy scissors");
+    let answer = prompt(`Would you like to buy ${tool}`);
     console.log(answer);
     if (answer.toLowerCase() === "y") {
       bankAccount -= price;
@@ -27,10 +29,8 @@ function buyTools(price) {
   }
 }
 
-while (bankAccount < 5) {
-  useTeeth();
-}
+useTool(5, tools[0]);
 
-buyTools(5);
+buyTools(5, tools[1]);
 
 console.log(bankAccount);
