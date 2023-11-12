d1ef23e9195b0f6f94b48bcfe569b4122174fa6d
ybuiko <juliabuiko@gmail.com>
2023-11-11 22:14:05 -0500

    landscaper: user can use push lawnmower to cut grass

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const prompt = require("prompt-sync")();
 
 //create function to use tolls to cut garss -- teeth
-const tools = ["teeth", "scissors"];
+const tools = ["teeth", "scissors", "old-timey push lawnmower"];
 let bankAccount = 0;
 
 console.log(
   " I am starting my landstpe businesss and I can cut your grass with my teath"
 );
 
-function useTool(count, tool) {
+function useTool(count, tool, price) {
   while (bankAccount < count) {
     let answer = prompt(`Would you like to have your grass cut with ${tool}?`);
     console.log(answer);
     if (answer.toLowerCase() === "y") {
-      bankAccount++;
+      bankAccount += price;
     }
   }
   console.log(bankAccount);
 }
 
 function buyTools(price, tool) {
-  if (bankAccount >= 5) {
+  if (bankAccount >= price) {
     let answer = prompt(`Would you like to buy ${tool}`);
     console.log(answer);
     if (answer.toLowerCase() === "y") {
@@ -29,8 +29,9 @@ function buyTools(price, tool) {
   }
 }
 
-useTool(5, tools[0]);
-
+useTool(5, tools[0], 1);
 buyTools(5, tools[1]);
+useTool(25, tools[1], 5);
+buyTools(25, tools[2]);
 
 console.log(bankAccount);
